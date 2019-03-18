7ed018b25f01fc8913c596e7bcf93bb02ef2808a
Pantelis Nasikas <pnasikas@gmail.com>
2019-03-18 16:00:42 +0200

    merge StateT conflict

--- src/Course/StateT.hs
+++ src/Course/StateT.hs
@@ -222,11 +222,7 @@ distinct' z = eval (filtering (\x -> State $ \s -> (S.notMember x s, S.insert x 
 -- >>> distinctF $ listh [1,2,3,2,1,101]
 -- Empty
 distinctF ::
-<<<<<<< HEAD
-  (Ord a , Num a) =>
-=======
   (Ord a, Num a) =>
->>>>>>> d82d69753bf7e8eac7c1d22733d688807c2b2065
   List a
   -> Optional (List a)
 distinctF z = evalT (filtering (\x  -> StateT $ \s -> 
